2711dff3346a23b1235a8b536f83fabb6283367b
Alex Faustino <faustino.alex@outlook.com>
2025-04-16 19:01:26 -0300

    feat: add a migration a new db

---
+++ prisma/migrations/20250416154425_add_new_tables/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "Producer" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "cpfCnpj" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Producer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Farm" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "totalArea" DOUBLE PRECISION NOT NULL,
+    "arableArea" DOUBLE PRECISION NOT NULL,
+    "vegetationArea" DOUBLE PRECISION NOT NULL,
+    "producerId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Farm_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Crop" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "season" TEXT NOT NULL,
+    "farmId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Crop_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Producer_cpfCnpj_key" ON "Producer"("cpfCnpj");
+
+-- AddForeignKey
+ALTER TABLE "Farm" ADD CONSTRAINT "Farm_producerId_fkey" FOREIGN KEY ("producerId") REFERENCES "Producer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Crop" ADD CONSTRAINT "Crop_farmId_fkey" FOREIGN KEY ("farmId") REFERENCES "Farm"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
